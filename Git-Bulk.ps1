0dc8996e06d0e6639f2b434ddc39ff9d4cc726b8
attilakrupl <krupl.attila@gmail.com>
2019-01-19 17:16:46 +0100

    "whitespaces fixed"

--- Git-Bulk.ps1
+++ Git-Bulk.ps1
@@ -1,25 +1,25 @@
 <#
 .SYNOPSIS
-	This script runs defined command in a defined parent directory of multiple git repositories recursively.
+    This script runs defined command in a defined parent directory of multiple git repositories recursively.
 .DESCRIPTION
-	The user can either apply selected git command in present working directory, or can provide a custom path for running git command.
+    The user can either apply selected git command in present working directory, or can provide a custom path for running git command.
 .PARAMETER -tragetpath 
-	The path of selected parent directory, otherwise it is the current working directory
+    The path of selected parent directory, otherwise it is the current working directory
 .PARAMETER -command 
-	Sepcifies the git command the user is about to run for each target repository. 
+    Sepcifies the git command the user is about to run for each target repository. 
 .NOTES
-	Version:        1.0
-  	Author:         Attila Krupl
-  	Creation Date:  13-01-2019
-  	Purpose/Change: Initial script development
+    Version:        1.0
+    Author:         Attila Krupl
+    Creation Date:  13-01-2019
+    Purpose/Change: Initial script development
 #>
 
 param
 (
-	[String]$targetpath=$pwd,
-	[String]$command="",
-	[Bool]$verbose=$true,
-	[Bool]$forcedexecution=$false
+    [String]$targetpath=$pwd,
+    [String]$command="",
+    [Bool]$verbose=$true,
+    [Bool]$forcedexecution=$false
 )
 
 [String]$gitfolder="\.git"
@@ -31,77 +31,75 @@ param
 # $args[2] - the value of the second placeholder
 function Write-Host-If-Verbose
 {
-	If($verbose -eq $true)
-	{
-		Write-Host($args[0] -f $args[1], $args[2]) -ForegroundColor Cyan
-	}
+    If($verbose -eq $true)
+    {
+        Write-Host($args[0] -f $args[1], $args[2]) -ForegroundColor Cyan
+    }
 }
 
 # Get all directories and hidden directories recursively within the given directory
 Get-ChildItem -Recurse -Attributes Directory,Directory+Hidden -Path $targetpath | % {
-	[String]$fullFolderName = $_.FullName
-	[String]$fullGitFolderPath = "$fullFolderName$gitfolder"
-	
-	# If the current folder contains the .git folder, then it is a git repository
-	If (Test-Path($fullGitFolderPath))
-	{
-		# If we are in a git repository, and we've got a command available we should run the given command
-		If ($command -ne "")
-		{
-			#Save current path
-			Push-Location $targetpath
+    [String]$fullFolderName = $_.FullName
+    [String]$fullGitFolderPath = "$fullFolderName$gitfolder"
 
-			#Change location to the git repositories folder
-			Set-Location -Path $fullFolderName
-
-			#If forced execution has been set, run command
-			If($forcedexecution -eq $true)
-			{
-				#If verbose mode have been set, log info
-				Write-Host-If-Verbose "Running command ({0}) in {1}" $command $fullFolderName
-				Invoke-Expression($command)
-			}
-			#If forced execution hasn't been set, ask user if he/she wants to run the command for the current repository
-			Else
-			{
-				[Bool]$isValidInput = $false
-				While (-Not $isValidInput)
-				{
-					[String]$runCommand = Read-Host -Prompt "Would you like to run command ($command) on $fullFolderName repository? (Yes (y), No (n), Cancel (c))"
-					If ($runCOmmand.ToLower() -eq "y")
-					{
-						#If verbose mode have been set, log info
-						$isValidInput = $true
-						Write-Host-If-Verbose "Running command ({0}) in {1}" $command $fullFolderName
-						Invoke-Expression($command)
-					}
-					ElseIf ($runCOmmand.ToLower() -eq "n")
-					{
-						#If verbose mode have been set, log info
-						$isValidInput = $true
-						Write-Host-If-Verbose "Skipping the running of command ({0}) in {1}" $command $fullFolderName
-					}
-					ElseIf ($runCOmmand.ToLower() -eq "c")
-					{
-						#If verbose mode have been set, log info
-						$isValidInput = $true
-						Write-Host-If-Verbose "Cancel running command ({0}) on repositories of following directory: {1}" $command $targetpath 
-						Pop-Location
-						Exit
-					}
-					Else
-					{
-						#Invalid input, user should try again
-						Write-Host("Invalid input({0})! Please choose a valid option." -f $runCOmmand) -ForegroundColor Yellow 
-					}
-				}
-			}
-			#Get back to the original location
-			Pop-Location
-		}
-		Else
-		{
-			Write-Host("No command provided to run in {0}" -f $fullFolderName) -ForegroundColor Magenta 
-		}
-	}
+    # If the current folder contains the .git folder, then it is a git repository
+    If (Test-Path($fullGitFolderPath))
+    {
+        # If we are in a git repository, and we've got a command available we should run the given command
+        If ($command -ne "")
+        {
+            #Save current path
+            Push-Location $targetpath   
+            #Change location to the git repositories folder
+            Set-Location -Path $fullFolderName  
+            #If forced execution has been set, run command
+            If($forcedexecution -eq $true)
+            {
+                #If verbose mode have been set, log info
+                Write-Host-If-Verbose "Running command ({0}) in {1}" $command $fullFolderName
+                Invoke-Expression($command)
+            }
+            #If forced execution hasn't been set, ask user if he/she wants to run the command for the current repository
+            Else
+            {
+                [Bool]$isValidInput = $false
+                While (-Not $isValidInput)
+                {
+                    [String]$runCommand = Read-Host -Prompt "Would you like to run command ($command) on $fullFolderName repository? (Yes (y), No (n), Cancel (c))"
+                    If ($runCOmmand.ToLower() -eq "y")
+                    {
+                        #If verbose mode have been set, log info
+                        $isValidInput = $true
+                        Write-Host-If-Verbose "Running command ({0}) in {1}" $command $fullFolderName
+                        Invoke-Expression($command)
+                    }
+                    ElseIf ($runCOmmand.ToLower() -eq "n")
+                    {
+                        #If verbose mode have been set, log info
+                        $isValidInput = $true
+                        Write-Host-If-Verbose "Skipping the running of command ({0}) in {1}" $command $fullFolderName
+                    }
+                    ElseIf ($runCOmmand.ToLower() -eq "c")
+                    {
+                        #If verbose mode have been set, log info
+                        $isValidInput = $true
+                        Write-Host-If-Verbose "Cancel running command ({0}) on repositories of following directory: {1}" $command $targetpath 
+                        Pop-Location
+                        Exit
+                    }
+                    Else
+                    {
+                        #Invalid input, user should try again
+                        Write-Host("Invalid input({0})! Please choose a valid option." -f $runCOmmand) -ForegroundColor Yellow 
+                    }
+                }
+            }
+            #Get back to the original location
+            Pop-Location
+        }
+        Else
+        {
+            Write-Host("No command provided to run in {0}" -f $fullFolderName) -ForegroundColor Magenta 
+        }
+    }
 }
